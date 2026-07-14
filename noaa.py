a64df64fbd84a3a78f46d28326d509dcc45001ce
codesofnature <perplexityone@proton.me>
2026-07-14 13:47:54 -0400

    Update noaa.py

--- noaa.py
+++ noaa.py
@@ -388,6 +388,7 @@ def generate_map_html(radar_frames, mode="live", include_astronomy=True, include
 <html>
 <head>
     <link rel="manifest" href="manifest.json">
+    <link rel="apple-touch-icon" href="icon.png">
     <meta name="apple-mobile-web-app-capable" content="yes">
     <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
     <meta name="apple-mobile-web-app-title" content="Radar">
